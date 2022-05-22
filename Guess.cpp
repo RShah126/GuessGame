f8b9320b7e57c0f734f666627f744e0d58cffd3c
Ryan Shah <65997615+RShah126@users.noreply.github.com>
2022-05-22 19:10:45 -0400

    Update Guess.cpp

--- Guess.cpp
+++ Guess.cpp
@@ -10,12 +10,12 @@ int main() {
     
     int num;
     
-    int numbers[] = {1,2,3,4,5,6,7,8,9};
+    int numbers[] = {1,2,3,4,5,6,7,8,9}; //Gives you 9 numbers
 
     int generate;
 
-    for(int i = 0; i < 10; i++){
-        generate = numbers[i];
+    for(int i = 0; i < 10; i++){ //Goes one above and kind of confuses the array size to generate a random number in the 100s
+        generate = numbers[i]; //generates number
     }
 
     //cout << "Your generated number is " << generate << endl;
@@ -27,11 +27,11 @@ int main() {
         if(num < generate || num > generate){
             cout << "Keep Trying!" << endl;
             cout << "Please Enter a number" << endl;
-            cin >> num;
+            cin >> num; //Gathers input
         }
     }
 
-    cout << "Congrats you win!, your number is " << num << endl;
+    cout << "Congrats you win!, your number is " << num << endl; //Only says this after a win
 
 
    
